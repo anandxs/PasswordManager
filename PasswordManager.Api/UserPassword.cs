4aa6bdc0dd362ff8ffdf8597c39ba413c9ae18c3
Anand S <anandsatheeshk@gmail.com>
2024-01-21 14:19:06 +0530

    UserPasswords table added

---
+++ PasswordManager.Api/UserPassword.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PasswordManager.Api;
+
+public class UserPassword
+{
+    [Key]
+    public int Id { get; set; }
+    [ForeignKey(nameof(IdentityUser))]
+    public string UserId { get; set; } = null!;
+    public IdentityUser User { get; set; } = null!;
+    [Required(ErrorMessage = "Encrypted password is a required field.")]
+    public string? EncrptedPassword { get; set; }
+}
